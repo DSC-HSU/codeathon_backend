a59ff90f360f184a24d5cb3611f2bd951c655618
Lulu <duy.lh@itss.edu.vn>
2024-10-17 17:01:23 +0700

    update test func

--- domain/profile.go
+++ domain/profile.go
@@ -3,7 +3,7 @@ package domain
 type Profile struct {
 	Id            string `json:"id"`
 	Email         string `json:"email"`
-	Username      string `json:"username"`
+	FullName      string `json:"full_name"`
 	AvatarUrl     string `json:"avatar_url"`
 	LinkedDevPass string `json:"linked_dev_pass"`
 }
